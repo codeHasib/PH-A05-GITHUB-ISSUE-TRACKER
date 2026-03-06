934230294c107b723e4eb1cf30db4c0ac98aec8d
codeHasib <mohammadhasib9827@gmail.com>
2026-03-06 19:16:29 +0600

    add functionality for login page

--- scripts/script.js
+++ scripts/script.js
@@ -3,5 +3,24 @@ const adminPassword = "admin123";
 const body = document.body;
 
 if(body.id === "loginPage") {
-    
+    const userNameInput = document.querySelector("#nameInput");
+    const passwordInput = document.querySelector("#passwordInput");
+    const loginBtn = document.querySelector("#loginBtn");
+    loginBtn.addEventListener("click", ()=> {
+        const nameValue = userNameInput.value.trim();
+        const passwordValue = passwordInput.value.trim();
+        if(!nameValue || !passwordValue) {
+            alert("Please fill-up both username and password");
+            return;
+        };
+        if(nameValue !== adminName) {
+            alert("Incorrect username");
+            return;
+        }
+        if(passwordValue !== adminPassword) {
+            alert("Incorrect password");
+            return;
+        };
+        window.location.href = "./main.html";
+    });
 }
