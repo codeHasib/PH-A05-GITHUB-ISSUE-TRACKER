93d22a66fe79fa93fc22b44207193ddabb7f2a18
codeHasib <mohammadhasib9827@gmail.com>
2026-03-07 00:40:32 +0600

    add modal dynamically with id api

--- scripts/script.js
+++ scripts/script.js
@@ -33,6 +33,17 @@ if (body.id === "mainPage") {
   const totalIssuesDisplay = document.querySelector("#totalIssues");
   const divParent = document.querySelector("#parentDiv");
 
+  // Modal DOMS
+  // const modalBox = document.querySelector(".modal-box");
+  const modalTitle = document.querySelector("#modal-title");
+  const modalSpanStatus = document.querySelector("#modal-status");
+  const modalOpenedBy = document.querySelector("#modal-list-assignee");
+  const modalDesc = document.querySelector("#modal-desc");
+  const modalAssignee = document.querySelector("#modal-assignee");
+  const modalPriority = document.querySelector("#modal-priority-level");
+  const modalTime = document.querySelector("#modal-time");
+  const bugsDiv = document.querySelector("#bugs");
+
   let filterValue = "all";
 
   const filterBtns = document.querySelectorAll(".filterBtn");
@@ -45,30 +56,49 @@ if (body.id === "mainPage") {
     });
   });
 
-  searchInputBtn.addEventListener("click", ()=> {
+  searchInputBtn.addEventListener("click", () => {
     const value = searchInput.value.trim();
-    if(value) loadBySearch(value);
+    if (value) loadBySearch(value);
     else loadAllIssues();
   });
 
   async function loadAllIssues() {
+    divParent.innerHTML = `
+    <div class="col-span-4 flex items-center justify-center mt-20">
+    <span class="loading loading-ring loading-xl"></span>
+    </div>
+    `;
     let res = await fetch(
       "https://phi-lab-server.vercel.app/api/v1/lab/issues",
     );
     let data = await res.json();
     let issuesArr = data.data;
-    const openArr = issuesArr.filter(item=> item.status === "open");
-    const closedArr = issuesArr.filter(item=> item.status === "closed");
-    if(filterValue === "all") renderIssues(issuesArr);
-    else if(filterValue === "open") renderIssues(openArr);
-    else if(filterValue === "closed") renderIssues(closedArr);
+    const openArr = issuesArr.filter((item) => item.status === "open");
+    const closedArr = issuesArr.filter((item) => item.status === "closed");
+    if (filterValue === "all") renderIssues(issuesArr);
+    else if (filterValue === "open") renderIssues(openArr);
+    else if (filterValue === "closed") renderIssues(closedArr);
   }
 
   async function loadBySearch(value) {
-    let res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${value}`);
+    divParent.innerHTML = `
+      <div class="col-span-4 flex items-center justify-center mt-20">
+    <span class="loading loading-ring loading-xl"></span>
+    </div>
+    `;
+    let res = await fetch(
+      `https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${value}`,
+    );
     let data = await res.json();
     let issuesArr = data.data;
     renderIssues(issuesArr);
+  }
+
+  async function loadById(id) {
+    let res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`);
+    let data = await res.json();
+    let issueObj = data.data;
+    renderModal(issueObj);
   }
 
   loadAllIssues();
@@ -80,6 +110,10 @@ if (body.id === "mainPage") {
       arr.forEach((item) => {
         let cardDiv = document.createElement("div");
         cardDiv.classList.add("cards");
+        cardDiv.addEventListener("click", async () => {
+          await loadById(item.id);
+          my_modal_2.showModal();
+        });
         let secondDiv = document.createElement("div");
         secondDiv.classList.add("borderBottom");
         let imageAndRateDiv = document.createElement("div");
@@ -129,6 +163,38 @@ if (body.id === "mainPage") {
       });
     } else {
       totalIssuesDisplay.textContent = `${arr.length} Issues`;
+      divParent.innerHTML = `
+      <h1 class="text-center font-extrabold text-2xl col-span-4 mt-10">
+      No issues found!!!
+      </h1>
+      `;
     }
+  }
+
+  function renderModal(obj) {
+    modalTitle.textContent = obj.title;
+    if (obj.status === "open") modalSpanStatus.classList.add("good");
+    else modalSpanStatus.classList.add("high");
+    modalSpanStatus.textContent = obj.status;
+    modalOpenedBy.textContent = obj.author;
+    modalTime.textContent = obj.createdAt.split("T")[0];
+    modalDesc.textContent = obj.description;
+    modalAssignee.textContent = obj.author;
+    if (obj.priority === "high") modalPriority.classList.add("high");
+    else if (obj.priority === "medium") modalPriority.classList.add("medium");
+    else modalPriority.classList.add("low");
+    modalPriority.textContent = obj.priority;
+    bugsDiv.innerHTML = "";
+    obj.labels.forEach((label) => {
+      let newLabel = label.toUpperCase();
+      let span = document.createElement("span");
+      span.textContent = newLabel;
+      span.style.marginRight = "8px";
+      if (label === "bug") span.classList.add("high");
+      else if (label === "help wanted") span.classList.add("medium");
+      else if (label === "enhancement") span.classList.add("good");
+      else span.classList.add("normal");
+      bugsDiv.append(span);
+    });
   }
 }
